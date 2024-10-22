54f2ae0cd8cf77232b06d56784c8ecadba22c848
jeffreywangcf <jeffreywangcf@gmail.com>
2024-10-22 02:54:26 -0400

    done with kanbas

--- a1/src/Kanbas/Database/index.js
+++ a1/src/Kanbas/Database/index.js
@@ -2,5 +2,6 @@ import courses from "./courses.json";
 import navigationLinks from "./navigation_links.json";
 import modules from "./modules.json";
 import assignments from "./assignments.json";
+import people from "./people.json";
 
-export {courses, navigationLinks, modules, assignments};
+export {courses, navigationLinks, modules, assignments, people};
---
+++ a1/src/Utils/time.js
@@ -0,0 +1,5 @@
+export const prettyMinutes = (minutes) => {
+    const hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    return `${String(hours).padStart(2, '0')}:${String(remainingMinutes).padStart(2, '0')}:00`;
+};
