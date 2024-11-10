7217eb9fbdf280a1cc9dd6966a351e506042716b
jeffreywangcf <jeffreywangcf@gmail.com>
2024-11-10 17:40:05 -0500

    lab4 done

--- a1/src/Labs/Lab3/index.jsx
+++ a1/src/Labs/Lab3/index.jsx
@@ -29,50 +29,61 @@ import FunctionDestructing from "./FunctionDestructing";
 import Classes from "./Classes";
 import Styles from "./Styles";
 import Highlight from "./Highlight";
+import {useSelector} from "react-redux";
 
 import '../lab_styles.css'
 
 export default function Lab3() {
+    const {todos} = useSelector((state: any) => state.todosReducer);
     return (<div id="wd-lab3" className="max-w-4xl mx-auto space-y-8 lab_specific">
-            <h2 className="text-3xl font-bold mb-4">Lab 3</h2>
-            <h3>JavaScript</h3>
-            <VariablesAndConstants/>
-            <VariableTypes/>
-            <BooleanVariables/>
-            <IfElse/>
-            <TernaryOperator/>
-            <ConditionalOutputIfElse/>
-            <ConditionalOutputInline/>
-            <LegacyFunctions/>
-            <ArrowFunctions/>
-            <ImpliedReturn/>
-            <TemplateLiterals/>
-            <SimpleArrays/>
-            <ArrayIndexAndLength/>
-            <AddingAndRemovingDataToFromArrays/>
-            <ForLoops/>
-            <MapFunction/>
-            <FindFunction/>
-            <FindIndexFunction/>
-            <FilterFunction/>
-            <JsonStringify/>
-            <House/>
-            <TodoList/>
-            <Spreading/>
-            <Destructing/>
-            <FunctionDestructing/>
-            <DestructingImports/>
-            <Classes/>
-            <Styles/>
-            <Add a={3} b={4}/>
-            <h4>Square of 4</h4>
-            <Square>4</Square>
-            <hr/>
-            <Highlight>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe
-                totam
-                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident
-                voluptates.
-            </Highlight>
-        </div>);
+        <h2 className="text-3xl font-bold mb-4">Lab 3</h2>
+        <ul className="list-group">
+            {todos.map((todo: any) => (<li className="list-group-item" key={todo.id}>
+                {todo.title}
+            </li>))}
+        </ul>
+        <hr/>
+
+        <h3>JavaScript</h3>
+        <VariablesAndConstants/>
+        <VariableTypes/>
+        <BooleanVariables/>
+        <IfElse/>
+        <TernaryOperator/>
+        <ConditionalOutputIfElse/>
+        <ConditionalOutputInline/>
+        <LegacyFunctions/>
+        <ArrowFunctions/>
+        <ImpliedReturn/>
+        <TemplateLiterals/>
+        <SimpleArrays/>
+        <ArrayIndexAndLength/>
+        <AddingAndRemovingDataToFromArrays/>
+        <ForLoops/>
+        <MapFunction/>
+        <FindFunction/>
+        <FindIndexFunction/>
+        <FilterFunction/>
+        <JsonStringify/>
+        <House/>
+        <TodoList/>
+        <Spreading/>
+        <Destructing/>
+        <FunctionDestructing/>
+        <DestructingImports/>
+        <Classes/>
+        <Styles/>
+        <Add a={3} b={4}/>
+        <h4>Square of 4</h4>
+        <Square>4</Square>
+        <hr/>
+        <Highlight>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum,
+            saepe
+            totam
+            vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores,
+            provident
+            voluptates.
+        </Highlight>
+    </div>);
 }
--- a1/src/Labs/TOC.jsx
+++ a1/src/Labs/TOC.jsx
@@ -5,15 +5,21 @@ import { FaGithub } from 'react-icons/fa';
 
 export default function TOC() {
     const { pathname } = useLocation();
+    const labCount = 4;
     return (
         <ul className="menu bg-base-200 rounded-box w-56 space-y-1">
             <li className="menu-title">DIRECTORY</li>
             <li><Link to="/Labs">Labs</Link></li>
-            <li><Link className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`} to="/Labs/Lab1">Lab 1</Link></li>
-            <li><Link className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`} to="/Labs/Lab2">Lab 2</Link></li>
-            <li><Link className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`} to="/Labs/Lab3">Lab 3</Link></li>
-            <li><Link to="/Kanbas">Kanbas</Link></li>
 
+            {Array.from({length: labCount}, (_, i) => `Lab${i + 1}`).map((lab) => (
+                <li>
+                    <Link className={`nav-link ${pathname.includes(lab) ? "active" : ""}`} to={`/Labs/${lab}`}>
+                        {lab.replace("Lab", "Lab ")}
+                    </Link>
+                </li>
+            ))}
+
+            <li><Link to="/Kanbas">Kanbas</Link></li>
             <li>
                 <a href="https://github.com/jeffreywangcf/cs-4550-workspace" id="wd-github" target="_blank" rel="noopener noreferrer">
                     Repository on <FaGithub/>
